567fab8bc33d387366d6584bb43e7049ac5e3cdf
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-11-04 10:25:53 +0800

    Update DeleteActivity.java

--- DeleteActivity.java
+++ DeleteActivity.java
@@ -1,9 +1,11 @@
 package com.example.kondawms;
 
-import android.content.ClipData;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,7 +14,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
@@ -22,65 +24,88 @@ import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class DeleteActivity extends MainActivity {
-    private RecyclerView recyclerView;
-    private ItemAdapter itemAdapter;
-    private List<Items> itemsList;
+public class DeleteActivity extends MainActivity implements View.OnClickListener{
+    EditText bin_location_input;
+    Button btn;
+    ProgressDialog loading;
     private static final String DATA_URL = "http://192.168.0.79/select.php?id=";
     private static final String KEY_ITEM = "item";
     private static final String KEY_LOCATION = "location";
     private static final String KEY_TIME = "time";
     private static final String JSON_ARRAY = "result";
+    List<Items> items = new ArrayList<>();
+    RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item_list);
-
+        setContentView(R.layout.activity_delete);
         recyclerView = findViewById(R.id.recyclerList);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        itemsList = new ArrayList<>();
-
-        LoadAllItems();
-    }
-
-    private void LoadAllItems() {
-        JsonArrayRequest request = new JsonArrayRequest(DATA_URL, new Response.Listener<JSONArray>() {
-            @Override
-            public void onResponse(JSONArray array) {
-                for(int i=0;i<array.length();i++)
-                {
-                    JSONObject object = null;
-                    try {
-                        object = array.getJSONObject(i);
-                        String item = object.getString("item").trim();
-                        String location = object.getString("location").trim();
-                        String time = object.getString("time").trim();
-
-                        Items items = new Items();
-                        items.setItem(item);
-                        items.setLocation(location);
-                        items.setTime(time);
-                        itemsList.add(items);
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                }
-                itemAdapter = new ItemAdapter(DeleteActivity.this,itemsList);
-                recyclerView.setAdapter(itemAdapter);
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(DeleteActivity.this, error.toString(), Toast.LENGTH_SHORT).show();
+        bin_location_input=(findViewById(R.id.bin_location_input));
+        btn=(findViewById(R.id.button_id3));
+        final Button button = findViewById(R.id.delete_back_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
-        RequestQueue requestQueue = Volley.newRequestQueue(DeleteActivity.this);
-        requestQueue.add(request);
+        btn.setOnClickListener(this);
+
     }
 
+    private void getData() {
+        String id = bin_location_input.getText().toString().trim();
+        if(id.equals("")) {
+            Toast.makeText(this, "Scan Bin Location", Toast.LENGTH_LONG).show();
+            return;
+        }
+        loading = ProgressDialog.show(this, "Please wait...", "Fetching...",false,false);
+        String url = DATA_URL+bin_location_input.getText().toString().trim();
 
-}
+        StringRequest stringRequest = new StringRequest(url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                loading.dismiss();
+                showJSON(response);
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(DeleteActivity.this, error.getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                });
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
+    }
+
+    private void showJSON(String response) {
+        String item;
+        String location;
+        String time;
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray result = jsonObject.getJSONArray(JSON_ARRAY);
+            //StringBuilder output = new StringBuilder();
+            for(int i=0;i<result.length();i++) {
+                JSONObject itemData = result.getJSONObject(i);
+                item = itemData.getString(KEY_ITEM);
+                location = itemData.getString(KEY_LOCATION);
+                time = itemData.getString(KEY_TIME);
+                //output.append(String.format("\nItem:\t%s\nLocation:\t%s\nTime:\t%s\n", item, location, time));
+                // currently only works when there is more than 1 entry in a particular location
+                items.add(new Items(item, location, time));
+            }
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            recyclerView.setAdapter(new ItemAdapter(getApplicationContext(), items));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void onClick(View v) {
+        getData();
+    }
