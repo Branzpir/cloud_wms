49938b9210557d4d843d1b3c67b0da022bf75485
Branzpir <97573390+Branzpir@users.noreply.github.com>
2022-12-16 10:23:27 +0800

    Update cloudwms.php

--- cloudwms.php
+++ cloudwms.php
@@ -1,68 +1,56 @@
 <?php
-	date_default_timezone_set('Australia/Perth');
+	error_reporting(0);
 	require_once 'utility.php';
 	$C = connect();
-	if(isset($_POST['submit']))
-	{
-		sqlInsert($C, 'INSERT INTO item_log VALUES(NULL, ?, ?, ?)', 'sss', $_POST['item'], $_POST['loc'], date('Y/m/d h:i:s'));
-	}
-	
 ?>
-<!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css">
+</head>
+<style></style>
 <body>
-<style>
-	@media print{ .kPrint{display:none;} }
-</style>
+		<br><h2 style="text-align:center">Eurotech Product Lookup</h2>
+		<div class="container" style="text-align:center">
+		<div class="container" style="display-inline:block">
+		<form method='POST' action=''>
+			<input type='text' name='search' placeholder='Enter Product Code'>
+			<input type='submit' name='sub'>
+		</form>
 
-<h1>Search for product</h1>
-<form method='POST' action=''>
-	<input type='text' name='search'>
-	<input type='submit' name='sub'>
-</form>
-<h1>Print product</h1>
-<form method='POST' action=''>
-	<input type='text' name='item' style='text-transfrom:uppercase'>
-	<input type='text' name='quantity'>
-	<input type="submit" name="buscar"></input>
-	<?php if(isset($_POST['buscar'])) {
-		?><button onclick="window.print();" class="kPrint" name="print">Print</button>
 		<?php
-	}
+			if(isset($_POST['sub']))
+			{
+				$search = $_POST['search'];
+				if($res=sqlSelect($C, "SELECT item, location FROM item_log WHERE item LIKE '%$search%'"))
+				{
+					if($count1=$res->num_rows)
+					{
+						while(($res1=$res->fetch_object()))
+						{                                   
+						?>
+							<div class="col">
+							<?php echo $res1->item . '->' . $res1->location?>
+						</div><?php
+						} 
+					}
+					else
+					{
+						echo "<p class='message'>No results for this search entered</p>";
+						echo json_encode($time);
+					} 
+				}
+			}
 		?>
-</form>
-<?php
-if(isset($_POST['quantity']))
-{
- $item = $_POST['item'];
- $quantity = $_POST['quantity'];
- $time = time();
- echo "Item : $item<br> Quantity : $quantity <br> <img alt='".$item.$quantity.$time."' src='barcode.php?codetype=Code39&size=80&text=".$item.$quantity.$time."&print=true'/>";
-}
-?>
-<?php
-	if(isset($_POST['sub']))
-	{
-		$search = $_POST['search'];
-		if($res=sqlSelect($C, "SELECT item, location FROM item_log WHERE item LIKE '%$search%'"))
-		{
-			if($count1=$res->num_rows)
-        	{
-				while(($res1=$res->fetch_object()))
-				{                                   
-                ?>
-                    <div class="col-md-4">
-                    <?php echo $res1->item . ' ' . $res1->location?>
-                </div><?php
-            	} 
-        	}
-        	else
-        	{
-            	echo "<p class='message'>No results for this search entered</p>";
-        	} 
+		<br>
+		<a style="color:#000000; underline:" href="print.php"><h5>> Print Label</h5></a>
+		</div>
+		</div>
+	<script>
+	if ( window.history.replaceState ) {
+			window.history.replaceState( null, null, window.location.href );
 		}
-	}
-	
-?>
+	</script>	
 </body>
 </html>
